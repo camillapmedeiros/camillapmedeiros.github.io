d252ccce897801bed3d0d24840824b2f2e1e9bf2
Camilla <camillapmedeiros@gmail.com>
2018-03-09 02:23:03 -0300

    create header animation for inside pages

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,11 +1,19 @@
-$("header--trigger__medium").click(function() {
-  $('.header--full').toggleClass('.header--medium');
-});
-
-$("header--trigger__small").click(function() {
-  $('.header--medium').toggleClass('.header--small');
-});
-
-$("header--trigger__shadow").click(function() {
-  $('.header--small').toggleClass('.header--shadow');
+$(function(){
+ var shrinkHeader = 300;
+  $(window).scroll(function() {
+    var scroll = getCurrentScroll();
+      if ( scroll >= shrinkHeader ) {
+          $('.header').addClass('header__shrink');
+          $('.header--title').removeClass('header--title__large');
+          $('.header--title').addClass('header--title__small');
+        }
+        else {
+            $('.header').removeClass('header__shrink');
+            $('.header--title').removeClass('header--title__small');
+            $('.header--title').addClass('header--title__large');
+        }
+  });
+function getCurrentScroll() {
+    return window.pageYOffset || document.documentElement.scrollTop;
+    }
 });
