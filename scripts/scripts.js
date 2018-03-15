d64b645d9a4b22e2fff95c44489a8393d0f5ecb9
Camilla <camillapmedeiros@gmail.com>
2018-03-14 22:35:31 -0300

    add list for projects and snippets

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,19 +1,17 @@
 $(function(){
- var shrinkHeader = 300;
-  $(window).scroll(function() {
-    var scroll = getCurrentScroll();
-      if ( scroll >= shrinkHeader ) {
-          $('.header').addClass('header__shrink');
-          $('.header--title').removeClass('header--title__large');
-          $('.header--title').addClass('header--title__small');
+    var shrinkHeader = 75;
+    $(window).scroll(function() {
+        var scroll = getCurrentScroll();
+          if ( scroll >= shrinkHeader ) {
+                $('.header').addClass('header--shrink');
+                $('.header__title').removeClass('header__title--large').addClass('header__title--small');
+            }
+            else {
+                $('.header').removeClass('header--shrink');
+                $('.header__title').removeClass('header__title--small').addClass('header__title--large');
+            }
+    });
+    function getCurrentScroll() {
+        return window.pageYOffset || document.documentElement.scrollTop;
         }
-        else {
-            $('.header').removeClass('header__shrink');
-            $('.header--title').removeClass('header--title__small');
-            $('.header--title').addClass('header--title__large');
-        }
-  });
-function getCurrentScroll() {
-    return window.pageYOffset || document.documentElement.scrollTop;
-    }
 });
