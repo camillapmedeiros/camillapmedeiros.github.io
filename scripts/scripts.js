68b6f9e374175174bd6d6597629288fd4fd732dc
Camilla <camillapmedeiros@gmail.com>
2018-09-24 21:25:55 -0700

    work on shadows

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,6 +2,20 @@
 $(document).ready(function(){
     var height = window.innerHeight;
 
+    $('.navigation__link, .header__link').click(function(event) {
+        event.preventDefault();
+        newLocation = this.href;
+        $('body').fadeOut(800, newpage);
+    });
+    function newpage() {
+        window.location = newLocation;
+    }
+
+    $('.arrow a').click(function(event) {
+        event.preventDefault();
+        $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
+    });
+
     $(window).scroll(function() {
         event.preventDefault();
         if (window.scrollY >= height){
@@ -14,17 +28,16 @@ $(document).ready(function(){
         return window.pageYOffset || document.documentElement.scrollTop;
     }
 
-    $('.navigation__link, .header__link').click(function(event) {
+    $(window).scroll(function() {
         event.preventDefault();
-        newLocation = this.href;
-        $('body').fadeOut(800, newpage);
+        if (window.scrollY >= 200){
+            $('.header--medium').css("box-shadow", "0 10px 20px 0 rgba(90,122,190,0.05)");
+        } else {
+            $('.header--medium').css("box-shadow", "none");
+        }
     });
-    function newpage() {
-        window.location = newLocation;
+    function getCurrentScroll() {
+        return window.pageYOffset || document.documentElement.scrollTop;
     }
 
-  $('.arrow a').click(function(event) {
-    event.preventDefault();
-    $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
-  });
 });
